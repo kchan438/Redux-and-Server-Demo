2666a0c3ea4a2416f5ac54f7d893276fd5dc5711
Kevin Chan <kevinchan128@msn.com>
2021-04-14 21:36:57 -0700

    CRLF to LF on Test Files
    Changes made to all the test files and deleted getByText on ListingCreationForm.test.

--- src/components/ListingCreationForm.test.js
+++ src/components/ListingCreationForm.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, getByText } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 
